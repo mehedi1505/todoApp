adc835bd6bfc0196fca0987a563abce6a876ad73
mehedi1505 <mehedi.hasan89@gmail.com>
2024-07-04 15:25:20 +0600

    change config file

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,21 @@
 const nextConfig = {
     env:{
     MONGODB_URI:"mongodb+srv://mehedihasan89:HFu3OZ6rUvvsShQT@todos.qffikos.mongodb.net/todo?retryWrites=true&w=majority&appName=todos",
-    API_URL:"http://localhost:3000",
+    API_URL:"https://todo-app-zeta-lime-44.vercel.app/",
+    },
+
+    async headers() {
+        return [
+            {
+                // matching all API routes
+                source: "/api/:path*",
+                headers: [
+                    // other headers omitted for brevity...
+                    { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" }
+                ]
+            }
+        ]
     }
-
-
 
     
 };
