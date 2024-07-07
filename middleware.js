b18a61eb1bb52219978453bf51a848b3624c3ae3
mehedi1505 <92613900+mehedi1505@users.noreply.github.com>
2024-07-08 00:48:29 +0600

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -5,7 +5,16 @@ export function middleware(req) {
  const url = req.nextUrl.pathname;
 
     if(url?.startsWith("/api")){
-        NextResponse.next().headers.append("Access-Control-Allow-Origin","*")
+         const res = NextResponse.next()
+         res.headers.append('Access-Control-Allow-Credentials', "true")
+         res.headers.append('Access-Control-Allow-Origin', '*')
+         res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
+         res.headers.append(
+             'Access-Control-Allow-Headers',
+             'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+         )
+     
+         return res
       }
 }
 
